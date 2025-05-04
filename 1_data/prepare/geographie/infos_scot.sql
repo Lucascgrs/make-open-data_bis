2cd2cef1051c74f0ed0918f15675ea331159ee5e
francois-gonon <francois.gonon@epfedu.fr>
2025-05-04 12:17:15 +0200

    Changed col. main_departement to include the department where the scot has the most population in the case of multi-departement SCoT. Added tests for scot_code is geographie/schema.yml : unique, not null

--- 1_data/prepare/geographie/infos_scot.sql
+++ 1_data/prepare/geographie/infos_scot.sql
@@ -37,9 +37,38 @@ select
     -- Région (on suppose qu'un SCOT n'est que sur une seule région)
     MIN(code_region) as code_region,
     MIN(nom_region) as nom_region,
-    -- Département unique ou NULL si plusieurs
-    CASE WHEN COUNT(DISTINCT code_departement) = 1 THEN MIN(code_departement) ELSE NULL END as code_departement,
-    CASE WHEN COUNT(DISTINCT nom_departement) = 1 THEN MIN(nom_departement) ELSE NULL END as nom_departement,
+    -- Département principal :
+    -- Si un seul département, on prend ce département, sinon celui avec le plus d'habitants
+    (
+        CASE 
+            WHEN COUNT(DISTINCT code_departement) = 1 THEN MIN(code_departement)
+            ELSE (
+                SELECT code_departement FROM (
+                    SELECT code_departement, SUM(CAST(population AS NUMERIC)) as pop
+                    FROM scot_communes sc2
+                    WHERE sc2.scot_code = scot_communes.scot_code
+                    GROUP BY code_departement
+                    ORDER BY pop DESC NULLS LAST
+                    LIMIT 1
+                ) t
+            )
+        END
+    ) as code_departement,
+    (
+        CASE 
+            WHEN COUNT(DISTINCT nom_departement) = 1 THEN MIN(nom_departement)
+            ELSE (
+                SELECT nom_departement FROM (
+                    SELECT nom_departement, SUM(CAST(population AS NUMERIC)) as pop
+                    FROM scot_communes sc2
+                    WHERE sc2.scot_code = scot_communes.scot_code
+                    GROUP BY nom_departement
+                    ORDER BY pop DESC NULLS LAST
+                    LIMIT 1
+                ) t
+            )
+        END
+    ) as main_departement,
     -- Booléen multi_dpt
     (COUNT(DISTINCT code_departement) > 1) as multi_dpt,
     SUM(CAST(population AS NUMERIC)) as population,
