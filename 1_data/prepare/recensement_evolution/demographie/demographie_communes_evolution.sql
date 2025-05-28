40d589eb6c178eb3e7b256b0b3011ea36cf84451
Lucascgrs <lucascongras@hotmail.fr>
2025-05-28 19:32:06 +0200

    test on evolution commune demographie

--- 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
+++ 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
@@ -2,8 +2,17 @@
 
 {% set annees = [2016, 2017, 2018, 2019, 2020, 2021] %}
 
--- On crée un CTE par année
+-- On crée un seul WITH contenant tous les CTEs
 with
+cog_communes as (
+  select
+    code as code_commune,
+    nom as nom_commune,
+    departement as code_departement,
+    region as code_region
+  from {{ source('sources', 'cog_communes') }}
+),
+
 {% for annee in annees %}
 -- Deux derniers chiffres de l'année (ex: 18 pour 2018)
 {% set suffix = (annee|string)[2:] %}
@@ -11,7 +20,7 @@ with
 
 {% set cols = dbt_utils.get_filtered_columns_in_relation(
     relation=relation,
-    include=['^.{{ suffix }}'],  -- match 2e et 3e caractère = année
+    include=['^.{{ '.' }}{{ suffix }}'],
     exclude=[]
 ) %}
 
