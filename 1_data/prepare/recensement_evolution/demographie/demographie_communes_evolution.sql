dbd8a2ca0e3c08680e586daab19619ee9f76980f
Lucascgrs <lucascongras@hotmail.fr>
2025-05-28 19:34:31 +0200

    test on evolution commune demographie

--- 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
+++ 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
@@ -2,7 +2,7 @@
 
 {% set annees = [2016, 2017, 2018, 2019, 2020, 2021] %}
 
--- On crée un seul WITH contenant tous les CTEs
+-- Un seul bloc WITH
 with
 cog_communes as (
   select
@@ -14,29 +14,15 @@ cog_communes as (
 ),
 
 {% for annee in annees %}
--- Deux derniers chiffres de l'année (ex: 18 pour 2018)
-{% set suffix = (annee|string)[2:] %}
-{% set relation = source('sources', 'base_cc_evol_struct_pop_' ~ annee) %}
-
-{% set cols = dbt_utils.get_filtered_columns_in_relation(
-    relation=relation,
-    include=['^.{{ '.' }}{{ suffix }}'],
-    exclude=[]
-) %}
-
 base_{{ annee }} as (
     select
-        code_commune,
-        {% for col in cols %}
-            {{ col }} as {{ col[3:] }}{% if not loop.last %},{% endif %}
-        {% endfor %},
+        *,
         {{ annee }} as annee
-    from {{ relation }}
+    from {{ source('sources', 'base_cc_evol_struct_pop_' ~ annee) }}
 ){% if not loop.last %},{% endif %}
-
 {% endfor %}
 
--- Union finale
+-- Requête principale
 select * from base_2016
 {% for annee in annees[1:] %}
 union all
