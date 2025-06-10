9c7d619a8afd7aea608313bbe15ef4e467d391c0
Lucascgrs <lucascongras@hotmail.fr>
2025-06-10 17:17:15 +0200

    modifications on recensement evolution, demographie

--- 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
+++ 1_data/prepare/recensement_evolution/demographie/demographie_communes_evolution.sql
@@ -1,79 +1,48 @@
+-- models/1_data/prepare/recensement_evolution/demographie_communes.sql
 {{ config(materialized='table') }}
 
-{% set annees = range(2016, 2022) %}        {# 2016‒2021 inclus #}
+{# Paramètres #}
+{% set annees = range(2016, 2022) %}      {# 2016 → 2021 #}
 
-{% set champs_raw = dbt_utils.get_query_results("""
-    SELECT champ_insee, clef_json, base_source
-      FROM {{ source('sources', 'champs_categorises') }}
-     WHERE categorie = 'demographie'
-""") %}
+{# Récupère la table champs_categorises chargée depuis ton S3 #}
+{% set champs_raw = dbt_utils.get_query_results(
+    "select champ_insee, clef_json, base_source
+       from {{ source('sources', 'champs_categorises') }}
+      where categorie = 'demographie'"
+) %}
 
-{% set pop_champs = [] %}
-{% set fam_champs = [] %}
+{# Sépare les indicateurs par source #}
+{% set rp_pop = [] %}
+{% set rp_fam = [] %}
+
 {% for row in champs_raw %}
-    {% set rec = {'champ_insee': row[0], 'clef_json': row[1]} %}
+    {% set record = {'champ_insee': row[0], 'clef_json': row[1]} %}
     {% if row[2] == 'rp_population' %}
-        {% do pop_champs.append(rec) %}
+        {% do rp_pop.append(record) %}
     {% elif row[2] == 'rp_familles_menages' %}
-        {% do fam_champs.append(rec) %}
+        {% do rp_fam.append(record) %}
     {% endif %}
 {% endfor %}
 
-with
+with rp_population as (
 
-{% for annee in annees %}
-struct_pop_{{ annee }} as (
     select
-        lpad(cast(codgeo as text), 5, '0')              AS code_commune,
-        {{ generate_demographie_columns_year(
-               pop_champs, annee, 'sp' ~ annee) }}
-    from {{ source('sources', 'base_cc_evol_struct_pop_' ~ annee) }}  sp{{ annee }}
-),
-{% endfor %}
+        lpad(cast(code_commune_insee as text), 5, '0') as code_commune,
+        {{ generate_demographie_columns(rp_pop,  annees) }}
+    from {{ source('sources', 'rp_population') }}
 
-{% for annee in annees %}
-fam_men_{{ annee }} as (
+), rp_familles_menages as (
+
     select
-        lpad(cast(codgeo as text), 5, '0')              AS code_commune,
-        {{ generate_demographie_columns_year(
-               fam_champs, annee, 'fm' ~ annee) }}
-    from {{ source('sources', 'base_cc_coupl_fam_men_' ~ annee) }}     fm{{ annee }}
-){% if not loop.last %},{% endif %}
-{% endfor %}
+        lpad(cast(code_commune_insee as text), 5, '0') as code_commune,
+        {{ generate_demographie_columns(rp_fam,  annees) }}
+    from {{ source('sources', 'rp_familles_menages') }}
 
-, rp_population as (
-
-    {# Démarre sur l’année la plus ancienne… #}
-    select *
-    from struct_pop_2016
-
-    {# …et on ajoute chaque millésime suivant. #}
-    {% for annee in annees if annee != 2016 %}
-    full outer join struct_pop_{{ annee }} using (code_commune)
-    {% endfor %}
-)
-
-, rp_familles_menages as (
-
-    select *
-    from fam_men_2016
-    {% for annee in annees if annee != 2016 %}
-    full outer join fam_men_{{ annee }} using (code_commune)
-    {% endfor %}
 )
 
 select
     coalesce(rp_population.code_commune,
-             rp_familles_menages.code_commune) AS code_commune,
-
-    -- Colonnes Population 2016-2021
-    {% for annee in annees %}
-        {{ generate_demographie_columns_year(pop_champs, annee, 'rp_population') }},{% endfor %}
-
-    -- Colonnes Familles/Ménages 2016-2021
-    {% for annee in annees %}
-        {{ generate_demographie_columns_year(fam_champs, annee, 'rp_familles_menages') }}{% if not loop.last %},{% endif %}
-    {% endfor %}
-
+             rp_familles_menages.code_commune)                            as code_commune,
+    {{ generate_demographie_columns(rp_pop + rp_fam, annees) }}
 from rp_population
 full outer join rp_familles_menages using (code_commune);
