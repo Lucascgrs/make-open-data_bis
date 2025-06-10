9400cf320259c6dc78dc5e5ffdc7e29892df2201
Lucascgrs <lucascongras@hotmail.fr>
2025-06-10 17:19:07 +0200

    modifications on recensement evolution, demographie

--- 5_macros/recensement_evolution/demographie_utils.sql
+++ 5_macros/recensement_evolution/demographie_utils.sql
@@ -1,29 +1,23 @@
--- macros/demographie_utils.sql
-{% macro demographie_col_expr(champ_insee, clef_json, annee) %}
+{% macro demographie_col_expr(champ_insee, clef_json, annee, alias) %}
     {#
-        Transforme, par ex. :
-          champ_insee = 'POP_P', clef_json = 'pop_p', annee = 2018
-        en :
-          P18_POP as pop_p_2018
+        Construit, par ex. :
+        {{ demographie_col_expr('POP_P', 'pop_p', 2018, 'sp2018') }}
+        → sp2018.P18_POP AS pop_p_2018
     #}
     {% set parts  = champ_insee.split('_') %}
     {% set suffix = parts[0] %}
     {% set prefix = parts[1] | upper %}
-
-    {{ prefix }}{{ (annee|string)[-2:] }}_{{ suffix }}
-        as {{ clef_json }}_{{ annee }}
+    {{ alias }}.{{ prefix }}{{ (annee|string)[-2:] }}_{{ suffix }}
+        AS {{ clef_json }}_{{ annee }}
 {% endmacro %}
 
-{% macro generate_demographie_columns(champs, annees) %}
-    {# 
-        champs  : liste de dicts     [{'champ_insee': ..., 'clef_json': ...}, …]
-        annees  : iterable           [2016, 2017, …, 2021]
-    #}
-    {%- set cols = [] -%}
-    {%- for row in champs -%}
-        {%- for annee in annees -%}
-            {%- do cols.append(demographie_col_expr(row.champ_insee, row.clef_json, annee)) -%}
-        {%- endfor -%}
-    {%- endfor -%}
+{% macro generate_demographie_columns_year(champs, annee, alias) %}
+    {# Champs d’un **seul** millésime, avec l’alias de table donné #}
+    {% set cols = [] %}
+    {% for c in champs %}
+        {% do cols.append(
+            demographie_col_expr(c.champ_insee, c.clef_json, annee, alias)
+        ) %}
+    {% endfor %}
     {{ cols | join(',\n        ') }}
 {% endmacro %}
