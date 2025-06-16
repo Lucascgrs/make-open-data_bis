20c76e62155a4b5aa67f302031e7a8f51b09b3f5
Lucascgrs <lucascongras@hotmail.fr>
2025-06-16 17:32:43 +0200

    small modifications to add communes informations

--- 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_communes_evolution.sql
+++ 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_communes_evolution.sql
@@ -1,10 +1,26 @@
-{{ config(
-    materialized='table',
-    schema='prepare'
-) }}
+{{ config(materialized='table', schema='prepare') }}
 
-{{ generate_evolution_table(
-    category='Population_Generale',
-    start_year=2016,
-    end_year=2021
-) }}
+with cog_communes as (
+    select
+        code as code_commune,
+        nom as nom_commune,
+        departement as code_departement,
+        region as code_region
+    from {{ source('sources', 'cog_communes') }}
+),
+
+evolution_data as (
+    {{ generate_evolution_table(
+        category='Population_Generale',
+        start_year=2016,
+        end_year=2021
+    ) }}
+)
+
+select 
+    e.*,
+    c.nom_commune,
+    c.code_departement,
+    c.code_region
+from evolution_data e
+left join cog_communes c on e."CODGEO" = c.code_commune
