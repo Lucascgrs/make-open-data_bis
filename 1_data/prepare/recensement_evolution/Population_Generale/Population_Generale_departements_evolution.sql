5bf802480b55d3f8a584f922b0b1adb25bfc5f96
Lucascgrs <lucascongras@hotmail.fr>
2025-06-17 16:27:34 +0200

    add departement level

--- 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_departements_evolution.sql
+++ 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_departements_evolution.sql
@@ -1,7 +1,22 @@
 {{ config(materialized='table', schema='prepare') }}
 
+<<<<<<< HEAD
 with commune_data as (
     select * from {{ ref('Population_Generale_communes_evolution') }}
+=======
+with infos_communes as (
+    select 
+        ltrim(code_commune, '0') as code_commune,
+        nom_commune,
+        code_arrondissement,
+        code_departement,
+        code_region,
+        nom_departement,
+        nom_region,
+        "SCoT" as nom_scot,
+        "SIREN EPCI" as siren_epci
+    from {{ source('prepare', 'infos_communes') }}
+>>>>>>> edbf969 (add departement level)
 ),
 
 -- Get numeric columns dynamically for aggregation
@@ -25,6 +40,7 @@ numeric_columns as (
 )
 
 select 
+<<<<<<< HEAD
     code_departement as "CODGEO",
     nom_departement,
     code_region,
@@ -33,3 +49,15 @@ select
     "p_pop"
 from numeric_columns
 order by code_departement, "annee"
+=======
+    e.*,
+    i.nom_commune,
+    i.code_departement,
+    i.siren_epci,
+    i.nom_departement,
+    i.code_region,
+    i.nom_region,
+    i.nom_scot
+from evolution_data e
+left join infos_communes i on ltrim(e."CODGEO", '0') = i.code_commune
+>>>>>>> edbf969 (add departement level)
