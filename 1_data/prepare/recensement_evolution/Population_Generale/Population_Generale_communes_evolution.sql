9606a91509978573e93443ea2711a53cc2420ad9
Lucascgrs <lucascongras@hotmail.fr>
2025-06-18 21:06:12 +0200

    back in the past

--- 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_communes_evolution.sql
+++ 1_data/prepare/recensement_evolution/Population_Generale/Population_Generale_communes_evolution.sql
@@ -1,31 +1,58 @@
 {{ config(materialized='table', schema='prepare') }}
 
-with commune_data as (
-    select * from {{ ref('Population_Generale_communes_evolution') }}
+with cog_communes as (
+    select
+        ltrim(code, '0') as code_commune,
+        nom as nom_commune,
+        ltrim(departement, '0') as code_departement,
+        ltrim(region, '0') as code_region,
+        ltrim(arrondissement, '0') as arrondissement,
+        replace(siren::text, '.0', '') as siren  -- Conversion du SIREN en text sans le cast en integer
+    from {{ source('sources', 'cog_communes') }}
+),
+
+cog_departements as (
+    select
+        ltrim(code, '0') as code_departement,
+        nom as nom_departement
+    from {{ source('sources', 'cog_departements') }}
+),
+
+cog_regions as (
+    select
+        ltrim(code, '0') as code_region,
+        nom as nom_region,
+        "chefLieu" as CodeChefLieu
+    from {{ source('sources', 'cog_regions') }}
+),
+
+communes_to_scot as (
+    select 
+        "SIREN EPCI" as siren_epci,
+        "SCoT" as nom_scot,
+        "Id SCoT" as code_scot
+    from {{ source('sources', 'communes_to_scot') }}
+),
+
+evolution_data as (
+    {{ generate_evolution_table(
+        category='Population_Generale',
+        start_year=2016,
+        end_year=2021
+    ) }}
 )
 
 select 
-    code_departement,
-    code_region,
-    nom_departement,
-    nom_region,
-    annee
-    {%- set columns = adapter.get_columns_in_relation(ref('Population_Generale_communes_evolution')) %}
-    {%- for col in columns %}
-    {%- if col.name not in ['CODGEO', 'annee', 'code_departement', 'code_region', 'nom_departement', 'nom_region', 'code_commune', 'nom_commune', 'nom_scot', 'siren_epci'] %}
-    ,sum(case when "{{ col.name }}" ~ '^[0-9]+\.?[0-9]*$' 
-        then nullif("{{ col.name }}", '')::numeric 
-        else null end) as "{{ col.name }}"
-    {%- endif %}
-    {%- endfor %}
-from commune_data
-group by 
-    code_departement,
-    code_region,
-    nom_departement,
-    nom_region,
-    annee
-order by 
-    code_region,
-    code_departement,
-    annee
+    e.*,
+    c.nom_commune,
+    c.code_departement,
+    c.siren,
+    d.nom_departement,
+    c.code_region,
+    r.nom_region,
+    s.nom_scot,
+    s.code_scot
+from evolution_data e
+left join cog_communes c on ltrim(e."CODGEO", '0') = c.code_commune
+left join cog_departements d on c.code_departement = d.code_departement
+left join cog_regions r on c.code_region = r.code_region
